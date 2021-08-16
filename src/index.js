dd1fa74caf935d5a9b5b737533fc52b5bc3d322e
el-mouse <elena.bo.d8@gmail.com>
2021-08-17 00:48:04 +0300

    init: start task Human Readable Number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function toReadable (number) {
-  
+    const units = [
+        '',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen'
+    ];
+    const dozens = [
+        '',
+        '',
+        'twenty',
+        'thirty',
+        'forty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
+        'ninety',
+    ];
+    const str = number.toString();
+    if (number === 0) {return 'zero';} // 0
+    if (number < 20) {return units[number];} // 1 - 19
+    if (str.length === 2) {return (dozens[str[0]] + ' ' + units[str[1]]).trim();} // 20 - 99
+    if (str.length == 3) {
+        if (str[1] === '0' && str[2] === '0') //100, 200, 300 - 900
+        {return units[str[0]] + ' hundred';}
+        else
+        {return units[str[0]] + ' hundred ' + toReadable(+(str[1] + str[2])).trim();} // 101 - 999
+    }
+
 }
+
